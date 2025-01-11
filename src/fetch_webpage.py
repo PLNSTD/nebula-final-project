21c3e8f99d05e21556f5aa6f73b1cd406ab5f552
Plidher <65611683+PLNSTD@users.noreply.github.com>
2025-01-11 17:20:15 -0500

    Successfully added Air Quality Index request per city

--- src/fetch_webpage.py
+++ src/fetch_webpage.py
@@ -3,7 +3,7 @@ import requests
 def fetch_page_content(url):
     response = requests.get(url)
     if response.status_code == 200:
-        return response.text
+        return response
     else:
         raise Exception(f'\n\tFailed to fetch web page: {url}.\n\tStatus code: {response.status_code}\n')
     
--- src/parse_cities.py
+++ src/parse_cities.py
@@ -1,25 +1,32 @@
 from bs4 import BeautifulSoup
 import fetch_webpage as fw
+import fetch_air_quality as faiq
 
 common_url = 'https://worldpopulationreview.com/countries/'
 
-def parse_largest_cities_data(country = None):
+def parse_country_largest_cities(country = None):
     if not country:
         raise Exception('\n\tNo country provided to parse data from\n')
 
-    html_content = fw.fetch_page_content(common_url + country)
+    html_content = fw.fetch_page_content(common_url + country).text
     soup = BeautifulSoup(html_content, 'html.parser')
     table_content = soup.find('tbody', class_='relative z-10 text-sm')
     table_rows = table_content.find_all('tr', class_='table-row')
+
     cities = []
     cities_population = []
+    air_quality_indexes = []
+
     for row in table_rows:
         city = row.find('th').get_text()
         population = row.find('td').get_text()
+        aqi = faiq.parse_city_air_quality(city)
+        air_quality_indexes.append(aqi)
         cities.append(city)
         cities_population.append(population)
     
-    return (cities, cities_population)
+    return (cities, cities_population, air_quality_indexes)
 
 # GOOD 
-parse_largest_cities_data()
+cities_info = parse_country_largest_cities('italy')
+print(cities_info)
